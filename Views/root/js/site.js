9f6aac2fa37021be67d404234a725ed177d0271d
slammedboi <sean_vw@yahoo.com>
2024-04-04 01:32:52 -0700

    testing

--- Views/root/js/site.js
+++ Views/root/js/site.js
@@ -1,8 +1,9 @@
 function sign_in()
 {
-    var signInForm = document.getElementById("SignInForm")
+    let signInForm = document.getElementById("SignInForm")
+
     var username = signInForm.username.value
     var password = signInForm.password.value
 
-    alert(username + " " + password)
+    
 }
